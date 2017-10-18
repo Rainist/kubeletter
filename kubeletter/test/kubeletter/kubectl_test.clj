79304f90da96c05cf315885cfe1b5e63d2f0aecb
Heechul Ryu <ryuhcii@gmail.com>
2017-10-18 11:01:57 +0900

    make tests work without depends on kubectl working

--- kubeletter/test/kubeletter/kubectl_test.clj
+++ kubeletter/test/kubeletter/kubectl_test.clj
@@ -6,18 +6,21 @@
   @(:exit-code result))
 
 (deftest success-test
-  (testing "fail"
-    (is (not (= 0
-           (exit-code (kubectl "fail"))))))
-  (testing "cluster-info"
-    (is (= 0
-           (exit-code (ping)))))
-  (testing "hpa"
-    (is (= 0
-           (exit-code (hpa "production")))))
-  (testing "top node"
-    (is (= 0
-           (exit-code (top-node)))))
-  (testing "top pod"
-    (is (= 0
-           (exit-code (top-pod "production"))))))
+  ;; UNCOMMENT ONLY IF YOUR INTERGRATION TESTING TOOLS PROVIDE HAS RIGHT ENVIRONMENT OR TO TEST LOCALLY
+  ;;
+  ;; (testing "fail"
+  ;;   (is (not (= 0
+  ;;          (exit-code (kubectl "fail"))))))
+  ;; (testing "cluster-info"
+  ;;   (is (= 0
+  ;;          (exit-code (ping)))))
+  ;; (testing "hpa"
+  ;;   (is (= 0
+  ;;          (exit-code (hpa "production")))))
+  ;; (testing "top node"
+  ;;   (is (= 0
+  ;;          (exit-code (top-node)))))
+  ;; (testing "top pod"
+  ;;   (is (= 0
+  ;;          (exit-code (top-pod "production")))))
+  )
--- kubeletter/test/kubeletter/parser_test.clj
+++ kubeletter/test/kubeletter/parser_test.clj
@@ -3,16 +3,89 @@
             [kubeletter.kubectl :refer :all]
             [kubeletter.parser :refer :all]))
 
-(defn out [result]
+(defn- out [result]
   (:out (:proc result)))
 
+(defn- parse-test [result]
+  (into-map (out result)))
+
 (deftest parser-test
+  ;; DO NOT UNCOMMENT TESTS IN THIS TEST
+  ;;
+  ;; (testing "top node"
+  ;;   (is (= (type [])
+  ;;          (type (into-map (out (top-node)))))))
+  ;; (testing "top pod"
+  ;;   (is (= (type [])
+  ;;          (type (into-map (out (top-pod "production")))))))
+  ;; (testing "hpa"
+  ;;   (is (= (type [])
+  ;;          (type (into-map (out (hpa "production")))))))
+  )
+
+(deftest parser-raw-test
   (testing "top node"
-    (is (= (type [])
-           (type (into-map (out (top-node)))))))
+    (is (=
+         (parse-test
+          {:proc
+           {:out
+            '("NAME                                               CPU(cores)   CPU%      MEMORY(bytes)   MEMORY%   "
+              "ip-172-20-62-138.ap-northeast-1.compute.internal   154m         7%        3827Mi          48%       "
+              "ip-172-20-39-187.ap-northeast-1.compute.internal   365m         9%        7480Mi          46%       "
+              )}})
+         '[{"NAME" "ip-172-20-62-138.ap-northeast-1.compute.internal",
+            "CPU(cores)" "154m",
+            "CPU%" "7%",
+            "MEMORY(bytes)" "3827Mi",
+            "MEMORY%" "48%"}
+           {"NAME" "ip-172-20-39-187.ap-northeast-1.compute.internal",
+            "CPU(cores)" "365m",
+            "CPU%" "9%",
+            "MEMORY(bytes)" "7480Mi",
+            "MEMORY%" "46%"}]
+         )))
+
   (testing "top pod"
-    (is (= (type [])
-           (type (into-map (out (top-pod "production")))))))
+    (is (=
+         (parse-test
+          {:proc
+           {:out
+            '("NAME                                                    CPU(cores)   MEMORY(bytes)   "
+              "simon-3770191713-xgpww                                  18m          303Mi           "
+              "banksalad-certificate-web-2605397671-ktb92              0m           120Mi           "
+              )}}
+          )
+         '[{"NAME" "simon-3770191713-xgpww",
+            "CPU(cores)" "18m",
+            "MEMORY(bytes)" "303Mi"}
+           {"NAME" "banksalad-certificate-web-2605397671-ktb92",
+            "CPU(cores)" "0m",
+            "MEMORY(bytes)" "120Mi"}]
+         )))
+
   (testing "hpa"
-    (is (= (type [])
-           (type (into-map (out (hpa "production"))))))))
+    (is (=
+         (parse-test
+          {:proc
+           {:out
+            '("NAME                                   REFERENCE                                         TARGETS      MINPODS   MAXPODS   REPLICAS   AGE"
+              "alfred                                 Deployment/alfred                                 17% / 50%    3         100       3          46d"
+              "alfred-legacy                          Deployment/alfred-legacy                          20% / 100%   4         4         4          46d"
+              )}}
+          )
+         '[{"NAME" "alfred",
+            "REFERENCE" "Deployment/alfred",
+            "TARGETS" "17% / 50%",
+            "MINPODS" "3",
+            "MAXPODS" "100",
+            "REPLICAS" "3",
+            "AGE" "46d"}
+           {"NAME" "alfred-legacy",
+            "REFERENCE" "Deployment/alfred-legacy",
+            "TARGETS" "20% / 100%",
+            "MINPODS" "4",
+            "MAXPODS" "4",
+            "REPLICAS" "4",
+            "AGE" "46d"}]
+         )))
+  )
